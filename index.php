d126f24245bc0b11e6fd4b19f327fbdc8b15a6d4
Sicky <90435780+JonathanSicky@users.noreply.github.com>
2023-03-10 14:43:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
     <body>
         <?php include 'navbar.php' ?>
-        <div id="myData"></div>
-        <canvas id="myChart"></canvas> 
+        <div>
+            <button onclick="showData()">Show Data</button>
+            <button onclick="hideData()">Hide Data</button>
+            <button onclick="showChart()">Show Chart</button>
+            <button onclick="hideChart()">Hide Chart</button>
+        </div>
+        <div id="myData" style="display:none;"></div>
+        <canvas id="myChart" style="display:none;"></canvas> 
         <script>
+            function showData() {
+                var data = document.getElementById("myData");
+                data.style.display = "block";
+            }
+
+            function hideData() {
+                var data = document.getElementById("myData");
+                data.style.display = "none";
+            }
+
+            function showChart() {
+                var chart = document.getElementById("myChart");
+                chart.style.display = "block";
+            }
+
+            function hideChart() {
+                var chart = document.getElementById("myChart");
+                chart.style.display = "none";
+            }
 
             fetch('https://api.punkapi.com/v2/beers')
             .then(response => response.json())
@@ -48,4 +72,3 @@
         </script>
     </body>
 </html>
-
